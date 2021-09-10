52ff9a048fe5558b08ee3d5091179d69685a6b9a
Nguyen Huu Nhan <82103041+NguyenHuuNhan1912@users.noreply.github.com>
2021-09-10 20:34:13 +0700

    Ngày sinh

--- Student_Management/date.py
+++ Student_Management/date.py
@@ -1,8 +1,10 @@
 class date:
+    
     def __init__(self,day,month,year):
         self.day=day
         self.month=month
         self.year=year
+        
     def show(self):
         f1='\x1b[6;37;41m'
         f2='\x1b[6;30;42m'
